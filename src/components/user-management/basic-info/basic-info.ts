a5c65702a7e15feb4f4de115ae6ed9a0ebf6c359
Jefferson Mantovani <jmantovani@alunos.utfpr.edu.br>
2018-05-29 23:06:02 -0300

    Preenchimento do formulário de autenticação quando o usuário já foi cadastrado.

--- src/components/user-management/basic-info/basic-info.ts
+++ src/components/user-management/basic-info/basic-info.ts
@@ -1,8 +1,8 @@
-import { Component, Input } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { UserServiceProvider } from '../../../providers/user-service/user-service';
-import { Events } from 'ionic-angular';
-import { AlertServiceProvider } from '../../../providers/alert-service/alert-service';
+import { Component, Input } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { UserServiceProvider } from "../../../providers/user-service/user-service";
+import { Events } from "ionic-angular";
+import { AlertServiceProvider } from "../../../providers/alert-service/alert-service";
 
 /**
  * Generated class for the BasicInfoComponent component.
@@ -11,13 +11,12 @@ import { AlertServiceProvider } from '../../../providers/alert-service/alert-ser
  * Components.
  */
 @Component({
-  selector: 'basic-info',
-  templateUrl: 'basic-info.html'
+  selector: "basic-info",
+  templateUrl: "basic-info.html"
 })
 export class BasicInfoComponent {
-
-  @Input() cellphone;
-  form: FormGroup
+  @Input() userInfo;
+  form: FormGroup;
 
   constructor(
     public formBuilder: FormBuilder,
@@ -25,16 +24,36 @@ export class BasicInfoComponent {
     public events: Events,
     public alertService: AlertServiceProvider
   ) {
-    this.form = this.formBuilder.group({
-      peopleId: [''],
-      name: ['', Validators.required],
-      userId: [''],
-      shortName: ['', Validators.required],
-      password: ['', Validators.compose([Validators.required, Validators.minLength(6), Validators.maxLength(6)])],
-      confirmPassword: ['', Validators.required],
-    }, { validator: this.matchingPasswords('password', 'confirmPassword') });
+    this.form = this.formBuilder.group(
+      {
+        peopleId: [""],
+        name: ["", Validators.required],
+        userId: [""],
+        shortName: ["", Validators.required],
+        password: [
+          "",
+          Validators.compose([
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(6)
+          ])
+        ],
+        confirmPassword: ["", Validators.required]
+      },
+      { validator: this.matchingPasswords("password", "confirmPassword") }
+    );
   }
 
+  ngAfterContentInit() {
+    console.log(this.userInfo);
+    if (this.userInfo.user) {
+      console.log(this.userInfo.user);
+      this.form.controls["peopleId"].setValue(this.userInfo.user.people._id);
+      this.form.controls["name"].setValue(this.userInfo.user.people.name);
+      this.form.controls["userId"].setValue(this.userInfo.user._id);
+      this.form.controls["shortName"].setValue(this.userInfo.user.shortName);
+    }
+  }
 
   matchingPasswords(passwordKey: string, confirmPasswordKey: string) {
     // TODO maybe use this https://github.com/yuyang041060120/ng2-validation#notequalto-1
@@ -46,30 +65,39 @@ export class BasicInfoComponent {
           mismatchedPasswords: true
         };
       }
-    }
+    };
   }
 
   updateUser() {
     if (this.form.valid) {
-      console.log('valid?');
+      console.log("valid?");
       console.log(this.form.value);
 
-      this.userService.update(this.form.value).then((result) => {
-        console.log(result);
-        if (result['success']) {
-          this.events.publish('app:userinfoupdated', { step: 'profile', statusProfile: false });
-          // Habilida a oção 'Perfil' do segment
-          // this.navCtrl.setRoot(HomePage, {}, {
-          //   animate: true,
-          //   direction: 'forward'
-          // })
-        }
-      }).catch((err) => {
-        // console.error(err);
-      });
+      this.userService
+        .update(this.form.value)
+        .then(result => {
+          console.log(result);
+          if (result["success"]) {
+            this.events.publish("app:userinfoupdated", {
+              step: "profile",
+              statusProfile: false
+            });
+            // Habilida a oção 'Perfil' do segment
+            // this.navCtrl.setRoot(HomePage, {}, {
+            //   animate: true,
+            //   direction: 'forward'
+            // })
+          }
+        })
+        .catch(err => {
+          // console.error(err);
+        });
     } else {
-      this.alertService.presentAlert('Informações incorretas', `Verifique usas informações básicas de usuário: Todos os campos são obrigatórios e a senha deve ter 6 caracteres`, 'OK');
+      this.alertService.presentAlert(
+        "Informações incorretas",
+        `Verifique usas informações básicas de usuário: Todos os campos são obrigatórios e a senha deve ter 6 caracteres`,
+        "OK"
+      );
     }
   }
-
 }
--- src/pages/user-basic-info/user-basic-info.ts
+++ src/pages/user-basic-info/user-basic-info.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, Platform, ModalController, Events } from 'ionic-angular';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { UserServiceProvider } from '../../providers/user-service/user-service';
-import { HomePage } from '../home/home';
-import { Keyboard } from '@ionic-native/keyboard';
+import { Component } from "@angular/core";
+import {
+  IonicPage,
+  NavController,
+  NavParams,
+  Platform,
+  ModalController,
+  Events
+} from "ionic-angular";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { UserServiceProvider } from "../../providers/user-service/user-service";
+import { HomePage } from "../home/home";
+import { Keyboard } from "@ionic-native/keyboard";
 
 /**
  * Generated class for the UserBasicInfoPage page.
@@ -19,12 +26,11 @@ export interface Slide {
 
 @IonicPage()
 @Component({
-  selector: 'page-user-basic-info',
-  templateUrl: 'user-basic-info.html',
+  selector: "page-user-basic-info",
+  templateUrl: "user-basic-info.html"
 })
 export class UserBasicInfoPage {
-
-  cellphone: string;
+  userInfo: any;
   slides: Slide[];
   form: FormGroup;
   isReady: boolean;
@@ -34,7 +40,7 @@ export class UserBasicInfoPage {
   statusLink = true;
   showFooter = true;
   showProfiles: Array<any>;
-  item = { title: '', component: 'ProfileCreatePage' };
+  item = { title: "", component: "ProfileCreatePage" };
   showHelp = false;
 
   constructor(
@@ -47,84 +53,104 @@ export class UserBasicInfoPage {
     public modalCtrl: ModalController,
     public events: Events
   ) {
-    this.events.subscribe('app:userinfoupdated', (userinfo) => {
-      console.log(userinfo);
-      this.statusProfile = userinfo['statusProfile'];
-      this.step = userinfo['step'];
+    this.events.subscribe("app:userinfoupdated", userinfo => {
+      // console.log(userinfo);
+      this.statusProfile = userinfo["statusProfile"];
+      this.step = userinfo["step"];
     });
-    console.log(this.navParams.get('user'));
-    if (this.navParams.get('user')) {
-      localStorage.setItem('userId', this.navParams.get('user')['profile']['user']['_id']);
-      this.showProfiles = new Array<{ type: string, component?: any, description?: any, icon?: string }>();
-      var profiles = this.navParams.get('user')['profile']['user']['profiles'];
+    // console.log(this.navParams.get("user"));
+    if (false) {
+      localStorage.setItem(
+        "userId",
+        this.navParams.get("user")["profile"]["user"]["_id"]
+      );
+      this.showProfiles = new Array<{
+        type: string;
+        component?: any;
+        description?: any;
+        icon?: string;
+      }>();
+      var profiles = this.navParams.get("user")["profile"]["user"]["profiles"];
       profiles.forEach(element => {
         var title = "";
-        if (element['type'] == 'countym') {
+        if (element["type"] == "countym") {
           title = "GESTÃO MUNICIPAL";
-        }
-        else if (element['type'] == 'schoolm') {
+        } else if (element["type"] == "schoolm") {
           title = "GESTÃO ESCOLAR";
-        }
-        else if (element['type'] == 'professor') {
+        } else if (element["type"] == "professor") {
           title = "PROFESSOR";
-        }
-        else if (element['type'] == 'parent') {
+        } else if (element["type"] == "parent") {
           title = "RESPONSÁVEL";
-        }
-        else if (element['type'] == 'student') {
+        } else if (element["type"] == "student") {
           title = "ALUNO";
         }
-        this.showProfiles.push(Object.assign(element, { title: title, component: 'ProfileEditPage', icon: 'assets/imgs/placeholder.png' }));
+        this.showProfiles.push(
+          Object.assign(element, {
+            title: title,
+            component: "ProfileEditPage",
+            icon: "assets/imgs/placeholder.png"
+          })
+        );
       });
-      this.events.publish('app:profiles', this.showProfiles);
-      var peopleId = this.navParams.get('user')['profile']['user']['people']['_id'];
-      var name = this.navParams.get('user')['profile']['user']['people']['name'];
-      var userId = this.navParams.get('user')['profile']['user']['_id'];
-      var shortName = this.navParams.get('user')['profile']['user']['shortName'];
+      this.events.publish("app:profiles", this.showProfiles);
+      var peopleId = this.navParams.get("user")["profile"]["user"]["people"][
+        "_id"
+      ];
+      var name = this.navParams.get("user")["profile"]["user"]["people"][
+        "name"
+      ];
+      var userId = this.navParams.get("user")["profile"]["user"]["_id"];
+      var shortName = this.navParams.get("user")["profile"]["user"][
+        "shortName"
+      ];
     }
-    this.cellphone = this.navParams.get('cellphone');
+    const user = this.navParams.get("user");
+    const _cellphone = this.navParams.get("cellphone");
+    this.userInfo = Object.assign({}, { cellphone: _cellphone, user });
+
     this.form = formBuilder.group({
       peopleId: [peopleId],
       name: [name, Validators.required],
       userId: [userId],
       shortName: [shortName, Validators.required]
     });
-    this.form.valueChanges.subscribe((v) => {
+    this.form.valueChanges.subscribe(v => {
       this.isReady = this.form.valid;
-    })
+    });
   }
 
   ionViewWillEnter() {
     this.slides = [
       {
         title: "Perfil para Alunos",
-        description: "Texto explicativo do uso de perfis para Alunos",
+        description: "Texto explicativo do uso de perfis para Alunos"
       },
       {
         title: "Perfil para Responsáveis",
-        description: "Texto explicativo do uso de perfis para Pais e Responsáveis",
+        description:
+          "Texto explicativo do uso de perfis para Pais e Responsáveis"
       },
       {
         title: "Perfil para Professores",
-        description: "Texto explicativo do uso de perfis para Professores",
+        description: "Texto explicativo do uso de perfis para Professores"
       },
       {
         title: "Perfil para Gestão Escolar",
-        description: "Texto explicativo do uso de perfis para Gestão Escolar",
+        description: "Texto explicativo do uso de perfis para Gestão Escolar"
       },
       {
         title: "Perfil para Administração Municipal",
-        description: "Texto explicativo do uso de perfis para Gestão Municipal",
+        description: "Texto explicativo do uso de perfis para Gestão Municipal"
       }
-    ]
-    this.step = this.navParams.get('step') || 'user';
-    if (this.step == 'user') {
+    ];
+    this.step = this.navParams.get("step") || "user";
+    if (this.step == "user") {
       this.statusUser = false;
     }
-    if (this.step == 'profile') {
+    if (this.step == "profile") {
       this.statusProfile = false;
     }
-    if (this.step == 'link') {
+    if (this.step == "link") {
       this.statusLink = false;
     }
   }
@@ -138,11 +164,15 @@ export class UserBasicInfoPage {
   }
 
   openModal(page) {
-    let profileModal = this.modalCtrl.create(page.component, { profile: page }, { enableBackdropDismiss: true });
+    let profileModal = this.modalCtrl.create(
+      page.component,
+      { profile: page },
+      { enableBackdropDismiss: true }
+    );
     profileModal.onDidDismiss(data => {
       if (data) {
-        if (data['profileType']) {
-          this.step = 'link';
+        if (data["profileType"]) {
+          this.step = "link";
           this.statusLink = true;
         }
       }
@@ -151,13 +181,16 @@ export class UserBasicInfoPage {
   }
 
   updateUser() {
-    this.userService.update(this.form.value).then((result) => {
-      // console.log(result);
-    }).catch((err) => {
-      // console.error(err);
-    });
+    this.userService
+      .update(this.form.value)
+      .then(result => {
+        // console.log(result);
+      })
+      .catch(err => {
+        // console.error(err);
+      });
 
-    this.step = 'profile';
+    this.step = "profile";
     this.statusProfile = false; // Habilida a oção 'Perfil' do segment
     // this.navCtrl.setRoot(HomePage, {}, {
     //   animate: true,
@@ -166,10 +199,13 @@ export class UserBasicInfoPage {
   }
 
   start() {
-    this.navCtrl.setRoot(HomePage, {}, {
-      animate: true,
-      direction: 'forward'
-    })
+    this.navCtrl.setRoot(
+      HomePage,
+      {},
+      {
+        animate: true,
+        direction: "forward"
+      }
+    );
   }
-
 }
